947322137408aa0d786ea65f3fcb1ead089538c4
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-01 02:55:59 +0600

    request and response

--- dist/app.js
+++ dist/app.js
@@ -5,10 +5,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join(__dirname, '../db/todo.json');
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World with express js');
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.get('/todos', (req, res) => {
+    const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+    console.log(data);
+    res.json();
 });
+app.post('/todos/create-todo', (req, res) => {
+    const data = req.body;
+    const { title, body } = req.body;
+    console.log(title, body);
+    res.json('hello');
+});
+exports.default = app;
+/**
+ * Basic File structure
+ * server - server handling like - starting, closing error handling of server. only related to server
+ * app file - routing handle, middleware, route related error
+ * app folder - app business logic handling like create read update delete, database related works
+ */ 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,30 @@
 
 import express, { Application, Request, Response } from 'express'
-const app : Application = express()
+const app: Application = express()
+const fs = require("fs")
+const path = require("path")
+const filePath = path.join(__dirname, '../db/todo.json')
+
+app.use(express.json())
 
 
-app.get('/', (req: Request, res : Response) => {
-  res.send('Hello World!')
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World with express js')
+})
+
+app.get('/todos', (req: Request, res: Response) => {
+    const data = fs.readFileSync(filePath, {encoding: "utf-8"})
+
+    console.log(data);
+    res.json()
+})
+
+app.post('/todos/create-todo', (req: Request, res: Response) => {
+    const data = req.body
+    const {title, body}= req.body
+
+    console.log(title, body);
+    res.json('hello')
 })
 
 
